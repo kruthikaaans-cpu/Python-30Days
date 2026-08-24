90741346249c96faa02e177f60f5cfcbd2d6f992
Kruthika N S <kruthikaaans@gmail.com>
2026-08-24 22:42:12 +0530

    day 2: lists in python

---
+++ Day2.py/prg1.py
@@ -0,0 +1,9 @@
+names = ["kruthika", "sai", "priya", "ananya"]
+print("my friends list:", names)
+print("first friend:", names[0])
+print("last friend:", names[-1])
+print("total friends:", len(names))
+names.append("sneha")
+print("updated friends list:", names)
+names.insert(1, "navya")
+print("friends list:", names)
